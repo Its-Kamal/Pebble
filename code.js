c334a3dedcc87f4ad4e6e9f0e32dd81895ca59cd
Kamal Singh <107418524+iamkamalsingh@users.noreply.github.com>
2022-09-04 09:27:17 +0530

    Update code.js

--- code.js
+++ code.js
@@ -14,3 +14,38 @@ function togglescrolly() {
         console.log(z);
     }
 }
+var Lname = document.querySelector('#signup label[for="name"]');
+var Lphone = document.querySelector('#signup label[for="phone"]');
+var namel = document.querySelector('#signup #name');
+var phonel = document.querySelector('#signup #phone');
+function form_onfocus(x) {
+    x.style.transform= "translateY(-2vh)";
+    x.style.fontSize='0.8vw';
+    x.style.color='white';
+}
+function form_onfocusout(y) {
+    if (y == Lname && namel.value.length>0 || y == Lphone && phonel.value.length>0 ) {
+        
+    } else {
+        y.style.transform= "translateY(0vh)";
+        y.style.fontSize='1vw';
+        y.style.color='#9daeb6';
+    }
+    
+}
+var check_box= document.querySelector('#checkbox');
+var checked= document.querySelector('#checkbox i');
+var n=0
+function check_the_box(){
+    if (n==0) {
+        checked.style.display='block';
+        check_box.style.border='0px solid black';
+        check_box.style.background='#0097DE';
+        n+=1;
+    } else {
+        checked.style.display='none';
+        check_box.style.border='2px solid white';
+        check_box.style.background='transparent';
+        n-=1;
+    }
+}
